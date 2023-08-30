2f58b61ddadf8e32a94893b769ac7d146523e034
Ellan <sharonstone0524@gmail.com>
2023-08-30 16:56:31 +0200

    create some new data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	let projects = ['with you', 'best roommate', 'yiui 7', 'best receipt', 'educate'];
+</script>
+
+<div id="banner" class="h-36 bg-gray-100">banner here</div>
+<div id="projects" class="gap-2 lg:grid lg:grid-cols-3 lg:gap-4 xl:grid-cols-4">
+	{#each projects as project}
+		<div class="project w-full bg-gray-200">
+			<p>{project}</p>
+		</div>
+	{/each}
+</div>
