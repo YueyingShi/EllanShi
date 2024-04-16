c84008001b56e28aa7ecea2ef00a19975bd0073b
YueyingShi <sharonstone0524@gmail.com>
2024-04-16 16:51:01 +0200

    testing still

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,34 @@
+<script lang="ts">
+	import { supabase } from '$lib/supabaseClient';
+	import { onMount } from 'svelte';
+	import { user } from '$lib/stores.js';
+	let projects: any = [];
+
+	import ProjectCard from '$lib/components/ProjectCard.svelte';
+	import FilterBadge from '$lib/components/atoms/FilterBadge.svelte';
+	import Banner from '$lib/components/Banner.svelte';
+	let selected: string = 'All';
+	let categories = ['All', 'Design', 'Research', 'Data', 'Development'];
+	import { fade } from 'svelte/transition';
+
+	function sortProject() {
+		projects.sort((a: any, b: any) => {
+			return a.id - b.id;
+		});
+	}
+	async function getAllProjects() {
+		try {
+			const { data } = await supabase.from('projects').select();
+			projects = data;
+			sortProject();
+		} catch (err) {
+			console.log(err);
+		}
+	}
+	onMount(async () => {
+		await getAllProjects();
+	});
+	$: console.log(projects);
+</script>
+
 hello
