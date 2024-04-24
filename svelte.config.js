33017435af824348fc2e500f05628fd5672705ba
YueyingShi <sharonstone0524@gmail.com>
2024-04-24 16:51:16 +0200

    runtime

--- svelte.config.js
+++ svelte.config.js
@@ -12,8 +12,8 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
 	},
-	preprocess: vitePreprocess()
-	// runtime: 'nodejs18.x'
+	preprocess: vitePreprocess(),
+	runtime: 'nodejs18.x'
 };
 
 export default config;
